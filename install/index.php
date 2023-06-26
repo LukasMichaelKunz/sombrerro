82829ae0fdea8797b5d65b5c7aed1c7b63a56a4f
kulu <lukas.kunz@adnovum.ch>
2023-06-26 11:00:32 +0200

    working

---
+++ install/index.php
@@ -0,0 +1,7 @@
+<?php
+    echo "<h2>MYSQL DATENBANK und USER erstellen:</h2>";
+    echo "<h3>create database sombrerro;</h3>";
+    echo "<h3>create user 'sombrerro'@'localhost' identified by 'leoandluke';</h3>"
+    echo "<h3>grant all on sombrerro.* to 'sombrerro'@'localhost' with grant option;</h3>"
+
+?>

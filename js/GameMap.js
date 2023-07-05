ecdd4d87d021a009b64821dcba1464c7ec97ca78
kulu <lukas.kunz@adnovum.ch>
2023-07-05 13:56:51 +0200

    5-7-2023

--- js/GameMap.js
+++ js/GameMap.js
@@ -2,12 +2,27 @@ class GameMap
 {
     constructor()
     {
-
+        alert ('con');
+        this.gameObjects = new Array();
+        this.gameObjects.push(new GameObject(60 ,'./image/testObject.png', 0.7, 0.3, 0.2, 0.2));
     }
-    drawObjectsToGame(object, ctx, left, top, width, height, gamePosX, gamePosY)
+    draw(ctx, left, top, width, height, gamePosX, gamePosY)
     {
-            var moveX = (object.width/2)*width;
-            var moveY = (object.height/2)*height;
-            ctx.drawImage(object.image, left + width * (object.positionX+gamePosX) - moveX, top + height * (object.positionY+gamePosY) - moveY, width * object.width, height * object.height);
+        this.gameObjects.forEach(element => 
+        {
+            var moveX = (element.width/2)*width;
+            var moveY = (element.height/2)*height;
+            ctx.drawImage(element.image, left + width * (element.positionX+gamePosX) - moveX, top + height * (element.positionY+gamePosY) - moveY, width * object.width, height * object.height);
+        });
+    }
+    isReady()
+    {
+        alert ('here');
+        var check=true;
+        this.gameObjects.forEach(element =>
+        {
+            if (!element.ready)check = false;
+        });
+        return check;
     }
 }
